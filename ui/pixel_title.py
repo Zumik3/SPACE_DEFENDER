eb4a3de706a4d826bb6ddf36a39e807ca340cab7
Zumik <johndoe@example.com>
2025-09-01 12:16:50 +0300

    Replace generated title with image from assets, support transparency

--- ui/pixel_title.py
+++ ui/pixel_title.py
@@ -1,156 +1,22 @@
 import pygame
+import os
 from utils.constants import *
 
 class PixelTitle:
     def __init__(self):
-        # Определяем пиксельное изображение для каждой буквы названия
-        # Более строгие и лаконичные буквы 5x7 пикселей
-        self.letters = {
-            'S': [
-                [0, 1, 1, 1, 0],
-                [1, 1, 0, 0, 0],
-                [1, 1, 0, 0, 0],
-                [0, 1, 1, 1, 0],
-                [0, 0, 0, 1, 1],
-                [0, 0, 0, 1, 1],
-                [1, 1, 1, 1, 0]
-            ],
-            'P': [
-                [1, 1, 1, 1, 0],
-                [1, 1, 0, 1, 1],
-                [1, 1, 0, 1, 1],
-                [1, 1, 1, 1, 0],
-                [1, 1, 0, 0, 0],
-                [1, 1, 0, 0, 0],
-                [1, 1, 0, 0, 0]
-            ],
-            'A': [
-                [0, 1, 1, 1, 0],
-                [1, 1, 0, 1, 1],
-                [1, 1, 0, 1, 1],
-                [1, 1, 1, 1, 1],
-                [1, 1, 0, 1, 1],
-                [1, 1, 0, 1, 1],
-                [1, 1, 0, 1, 1]
-            ],
-            'C': [
-                [0, 1, 1, 1, 0],
-                [1, 1, 0, 0, 0],
-                [1, 1, 0, 0, 0],
-                [1, 1, 0, 0, 0],
-                [1, 1, 0, 0, 0],
-                [1, 1, 0, 0, 0],
-                [0, 1, 1, 1, 0]
-            ],
-            'E': [
-                [1, 1, 1, 1, 1],
-                [1, 1, 0, 0, 0],
-                [1, 1, 0, 0, 0],
-                [1, 1, 1, 1, 0],
-                [1, 1, 0, 0, 0],
-                [1, 1, 0, 0, 0],
-                [1, 1, 1, 1, 1]
-            ],
-            'D': [
-                [1, 1, 1, 1, 0],
-                [1, 1, 0, 1, 1],
-                [1, 1, 0, 0, 1],
-                [1, 1, 0, 0, 1],
-                [1, 1, 0, 0, 1],
-                [1, 1, 0, 1, 1],
-                [1, 1, 1, 1, 0]
-            ],
-            'F': [
-                [1, 1, 1, 1, 1],
-                [1, 1, 0, 0, 0],
-                [1, 1, 0, 0, 0],
-                [1, 1, 1, 1, 0],
-                [1, 1, 0, 0, 0],
-                [1, 1, 0, 0, 0],
-                [1, 1, 0, 0, 0]
-            ],
-            'N': [
-                [1, 1, 0, 0, 1],
-                [1, 1, 1, 0, 1],
-                [1, 1, 1, 0, 1],
-                [1, 1, 0, 1, 1],
-                [1, 1, 0, 1, 1],
-                [1, 1, 0, 0, 1],
-                [1, 1, 0, 0, 1]
-            ],
-            'R': [
-                [1, 1, 1, 1, 0],
-                [1, 1, 0, 1, 1],
-                [1, 1, 0, 1, 1],
-                [1, 1, 1, 1, 0],
-                [1, 1, 0, 1, 0],
-                [1, 1, 0, 0, 1],
-                [1, 1, 0, 0, 1]
-            ],
-            ' ': [  # Пробел
-                [0, 0, 0, 0, 0],
-                [0, 0, 0, 0, 0],
-                [0, 0, 0, 0, 0],
-                [0, 0, 0, 0, 0],
-                [0, 0, 0, 0, 0],
-                [0, 0, 0, 0, 0],
-                [0, 0, 0, 0, 0]
-            ]
-        }
+        # Загружаем изображение названия игры
+        assets_dir = os.path.join(os.path.dirname(__file__), '..', 'assets')
+        self.title_image = pygame.image.load(os.path.join(assets_dir, 'title.png')).convert_alpha()
         
-        # Строгие космические цвета
-        self.primary_color = (100, 200, 255)    # Голубой
-        self.secondary_color = (200, 100, 255)  # Пурпурный
-        
-        # Размер пикселя и отступы
-        self.pixel_size = 5
-        self.letter_spacing = 6
-        self.word_spacing = 20
+        # Получаем размеры изображения
+        self.width = self.title_image.get_width()
+        self.height = self.title_image.get_height()
         
     def draw(self, screen, x, y):
-        # Рисуем название "SPACE DEFENDER" по центру
-        # Разделяем на два слова по центру экрана
-        space_word = "SPACE"
-        defender_word = "DEFENDER"
-        
-        # Вычисляем ширину каждого слова
-        space_width = len(space_word) * 5 * self.pixel_size + (len(space_word) - 1) * self.letter_spacing
-        defender_width = len(defender_word) * 5 * self.pixel_size + (len(defender_word) - 1) * self.letter_spacing
-        
-        # Центрируем каждое слово отдельно
-        space_x = (screen_width - space_width) // 2
-        defender_x = (screen_width - defender_width) // 2
-        
-        # Рисуем первое слово
-        current_x = space_x
-        for i, char in enumerate(space_word):
-            if char in self.letters:
-                self._draw_letter(screen, char, current_x, y, i)
-                current_x += len(self.letters[char][0]) * self.pixel_size + self.letter_spacing
-        
-        # Рисуем второе слово ниже
-        current_x = defender_x
-        for i, char in enumerate(defender_word):
-            if char in self.letters:
-                self._draw_letter(screen, char, current_x, y + 60, i + len(space_word))
-                current_x += len(self.letters[char][0]) * self.pixel_size + self.letter_spacing
-        
-    def _draw_letter(self, screen, letter, x, y, index):
-        # Получаем матрицу пикселей для буквы
-        pixel_matrix = self.letters[letter]
-        
-        # Выбираем цвет в зависимости от позиции буквы в названии
-        color = self.primary_color if index % 2 == 0 else self.secondary_color
-        
-        # Рисуем каждый пиксель буквы
-        for row in range(len(pixel_matrix)):
-            for col in range(len(pixel_matrix[row])):
-                if pixel_matrix[row][col] == 1:
-                    # Рисуем пиксель
-                    pixel_rect = pygame.Rect(
-                        x + col * self.pixel_size,
-                        y + row * self.pixel_size,
-                        self.pixel_size,
-                        self.pixel_size
-                    )
-                    pygame.draw.rect(screen, color, pixel_rect)
+        # Рисуем изображение названия игры
+        # Центрируем по экрану, если координаты (0, 0)
+        if x == 0 and y == 0:
+            x = (screen_width - self.width) // 2
+            y = screen_height // 2 - 150
+            
+        screen.blit(self.title_image, (x, y))
