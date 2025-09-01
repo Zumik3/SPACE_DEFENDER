83762a661351c9806ffae4318ea857669e5de8e1
Zumik <johndoe@example.com>
2025-09-01 16:59:17 +0300

    Add menu music support: Implement separate music tracks for menu and gameplay, improve music volume control, and enhance sound system management

--- utils/sound_manager.py
+++ utils/sound_manager.py
@@ -7,6 +7,7 @@ class SoundManager:
     def __init__(self, settings_manager=None):
         pygame.mixer.init()
         self.music_sound = None
+        self.menu_sound = None
         self.shoot_sound = None
         self.explosion_sound = None
         self.settings_manager = settings_manager
@@ -28,6 +29,7 @@ class SoundManager:
         script_dir = os.path.dirname(__file__)
         assets_dir = os.path.join(os.path.dirname(script_dir), 'assets')
         self.music_sound = pygame.mixer.Sound(os.path.join(assets_dir, "music.wav"))
+        self.menu_sound = pygame.mixer.Sound(os.path.join(assets_dir, "menu.wav"))
         self.shoot_sound = pygame.mixer.Sound(os.path.join(assets_dir, "shoot.wav"))
         self.explosion_sound = pygame.mixer.Sound(os.path.join(assets_dir, "explosion.wav"))
 
@@ -37,9 +39,26 @@ class SoundManager:
             self.music_sound.set_volume(self.music_volume)
             self.music_sound.play(loops)
 
+    def play_menu_music(self, loops=-1):
+        """Воспроизведение музыки меню"""
+        if self.menu_sound:
+            # Устанавливаем текущую громкость перед воспроизведением
+            self.menu_sound.set_volume(self.music_volume)
+            self.menu_sound.play(loops)
+
     def stop_music(self):
         if self.music_sound:
             self.music_sound.stop()
+
+    def stop_menu_music(self):
+        """Остановка музыки меню"""
+        if self.menu_sound:
+            self.menu_sound.stop()
+
+    def stop_all_music(self):
+        """Остановка всей музыки"""
+        self.stop_music()
+        self.stop_menu_music()
 
     def set_music_volume(self, volume):
         # Ограничиваем значение между 0 и 1
@@ -48,6 +67,8 @@ class SoundManager:
         self.music_volume = round(volume, 2)
         if self.music_sound:
             self.music_sound.set_volume(self.music_volume)
+        if self.menu_sound:
+            self.menu_sound.set_volume(self.music_volume)
         # Сохраняем настройку, если есть SettingsManager
         if self.settings_manager:
             self.settings_manager.set("music_volume", self.music_volume)
